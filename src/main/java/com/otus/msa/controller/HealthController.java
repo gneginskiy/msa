8a759d109b8fdd4a322447b85b227ed10771dbbc
gneginskiy <gneginskiy@mail.ru>
2023-12-20 23:29:33 +0400

    initial

--- src/main/java/com/otus/msa/controller/HealthController.java
+++ src/main/java/com/otus/msa/controller/HealthController.java
@@ -8,12 +8,9 @@ import org.springframework.web.bind.annotation.*;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController
-@RequestMapping(HealthController.HEALTH_API_URL)
 @RequiredArgsConstructor
 public class HealthController {
-    public static final String HEALTH_API_URL = "/health/";
-
-    @GetMapping(produces = APPLICATION_JSON_VALUE)
+    @GetMapping(path = {"/health/", "/health"}, produces = APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.CREATED)
     public HealthDto create() {
         return new HealthDto().setStatus(HealthDto.HealthStatus.OK);
